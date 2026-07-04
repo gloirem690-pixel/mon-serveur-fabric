de676be8a69e0ccc8514f315f85b20da41d1e7e7
gloirem690-pixel <gloirem690@gmail.com>
2026-07-04 02:03:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Aller dans le répertoire du serveur
+# Aller dans le dossier du serveur
 cd server-files
 
-# Démarrer le serveur Fabric en arrière-plan
-java -Xmx2048M -Xms1024M -jar fabric-server-launch.jar nogui &
+# Télécharger server.jar s'il n'existe pas
+if [ ! -f "server.jar" ]; then
+    echo "Téléchargement de server.jar..."
+    wget -O server.jar https://piston-data.mojang.com/v1/objects/95d414bf4b0c9db5ab0a24c3d4020b523c5e265d/server.jar
+fi
 
-# Revenir au répertoire racine pour lancer le panel
-cd ..
-node server.js
+# Lancer le serveur Fabric
+java -Xmx2048M -Xms1024M -jar fabric-server-launch.jar nogui
