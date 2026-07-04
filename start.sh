58aaccaf9d620cfd69db15e09be6b56cc62b9ba1
gloirem690-pixel <gloirem690@gmail.com>
2026-07-04 04:02:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,11 @@ if [ ! -f "server.jar" ]; then
     wget -O server.jar https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar
 fi
 
+if [ ! -f "fabric-server-launch.jar" ]; then
+    echo "Téléchargement de fabric-server-launch.jar..."
+    wget -O fabric-server-launch.jar https://maven.fabricmc.net/net/fabricmc/fabric-loader/0.19.3/fabric-loader-0.19.3.jar
+fi
+
 java -Xmx2048M -Xms1024M -jar fabric-server-launch.jar nogui &
 
 cd ..
